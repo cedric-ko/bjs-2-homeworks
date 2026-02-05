8246c8df5204259c36c4c5df84b6e605619d7398
Alexey Utkin <cedrickobayashi@gmail.com>
2026-02-05 22:54:12 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,7 +2,28 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
-  return arr;
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return arr; 
+  }
+    
+  if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x); 
+    return arr;
+  }
+    
+  if (d > 0) {
+    let dSqrRoot = Math.sqrt(d);
+        
+    let x1 = (-b + dSqrRoot) / (2 * a);
+    let x2 = (-b - dSqrRoot) / (2 * a);
+        
+    arr.push(x1); 
+    arr.push(x2); 
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
