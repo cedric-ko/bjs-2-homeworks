4aebb7bd5ac2e6a45c92e393df0883052affc8b1
Alexey Utkin <cedrickobayashi@gmail.com>
2026-02-08 19:28:12 +0300

    Задание 2, задача 2 - выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -21,19 +21,59 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) { // проверка наличия элементов в массиве
+    return 0; // если в массиве 0 элементов, возвращаем 0
+  }
+  
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0) { // проверка наличия элементов в массиве
+    return 0; // если в массиве 0 элементов, возвращаем 0
+  }
+  
+  const max = Math.max(...arr); // вычисляем максимальное из значений элементов массива
+  const min = Math.min(...arr); // вычисляем минимальное из значений элементов массива
+  
+  return max - min; // возвращаем разницу значений
 }
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) { // проверка наличия элементов в массиве
+    return 0; // если в массиве 0 элементов, возвращаем 0
+  }
 
+  let sumEvenElement = 0;  // инициализируем переменную с суммой чётных значений, равной нулю
+  let sumOddElement = 0;   // инициализируем переменную с суммой нечётных значений, равной нулю
+  
+  for (let i = 0; i < arr.length; i++) { // проходим по элементам массива
+    if (arr[i] % 2 === 0) { // если значение чётное, то есть остаток от деления на 2 равен 0
+      sumEvenElement += arr[i]; // прибавляем его к сумме чётных значений 
+    } else {
+      sumOddElement += arr[i]; // иначе прибавляем его к сумме нечётных значений
+    }
+  }
+  
+  return sumEvenElement - sumOddElement; // возвращаем разницу сумм чётных и нечётных значений
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) { // проверка наличия элементов в массиве
+    return 0; // если в массиве 0 элементов, возвращаем 0
+  }
 
+  let sumEvenElement = 0;  // инициализируем переменную с суммой чётных значений, равной нулю
+  let countEvenElement = 0; // инициализируем переменную с количеством чётных значений, равным нулю
+  
+  for (let i = 0; i < arr.length; i++) { // проходим по элементам массива
+    if (arr[i] % 2 === 0) { // если значение чётное
+      sumEvenElement += arr[i]; // прибавляем его к сумме чётных значений
+      countEvenElement++; // и увеличиваем счётчик чётных значений на 1
+    }
+  }
+  
+  return sumEvenElement / countEvenElement; // возвращаем среднее значение (делим сумму на количество значений)
 }
 
 function makeWork (arrOfArr, func) {
